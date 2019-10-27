4aa73aca7872e05340b85c0880911277a8582e23
Michael Haidar <mhaidar@csumb.edu>
2019-10-26 20:41:41 -0700

    second commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+
+//routes
+app.get("/", function(req, res){
+    res.render("index.html");
+});
+
+app.get("/mercury", function(req, res){
+    res.render("mercury.html");
+});
+
+app.get("/venus", function(req, res){
+    res.render("venus.html");
+});
+
+app.get("/earth", function(req, res){
+    res.render("earth.html");
+});
+
+app.get("/home", function(req, res){
+    res.render("index.html");
+});
+
+//server listener
+app.listen("8081","127.0.0.1", function(){
+    console.log("Express Server is running....");
+
+});
